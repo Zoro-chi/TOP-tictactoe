cbedc7aa47caca6c0d8cd2e3a058286b2730a18a
zoro <jagbetuyi001@gmail.com>
2022-04-16 20:36:27 +0100

    backbone

---
+++ main.js
@@ -0,0 +1,17 @@
+
+const gameBoard = (() => {
+
+    let gameBoard = []
+
+})()
+
+const game = (() => {
+
+})()
+
+const player = ((name) => {
+    return {
+        playerName : name,
+
+    }
+})

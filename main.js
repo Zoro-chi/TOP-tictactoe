133e1547c3c920debaef81194f2e388ad521ad67
zoro <jagbetuyi001@gmail.com>
2022-04-18 17:39:12 +0100

    added turn based and checkwin functionality

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+let turn = document.querySelector(".turn")
 const player1 = new Player("player1", "X") 
 const player2 = new Player("player2", "O")
-let currentPlayer = player1;
+let currentPlayer = player1
+turn.innerText = `${currentPlayer.name}`
 
 const gameBoard = (() => {
 
@@ -29,12 +31,47 @@ const game = (() => {
     const gameBoardArr = gameBoard.gameBoardArr
     
     const posclicked = (event) => {
-    const id = event.target.dataset.position
-    if(!gameBoard.spaces[(+id - 1)]){
-        gameBoard.spaces[(+id - 1)] = currentPlayer
-        event.target.innerText = currentPlayer.mark
-        currentPlayer = currentPlayer == player1 ? player2 : player1;
-    }
+        const id = event.target.dataset.position
+        const playerHasWon = () =>{
+            if (gameBoard.spaces[0] === currentPlayer.mark) {
+                if (gameBoard.spaces[1] == currentPlayer.mark && gameBoard.spaces[2] == currentPlayer.mark){
+                    return turn.innerText = `${currentPlayer.name} wins up top`
+                }
+                if (gameBoard.spaces[3] == currentPlayer.mark && gameBoard.spaces[6] == currentPlayer.mark){
+                    return turn.innerText = `${currentPlayer.name} wins on the left`
+                }
+                if (gameBoard.spaces[4] == currentPlayer.mark && gameBoard.spaces[8] == currentPlayer.mark){
+                    return turn.innerText = `${currentPlayer.name} wins diagonally`
+                }
+            }
+            if (gameBoard.spaces[8] === currentPlayer.mark){
+                if (gameBoard.spaces[2] == currentPlayer.mark && gameBoard.spaces[5] == currentPlayer.mark){
+                    return turn.innerText = `${currentPlayer.name} wins on the right`
+                }
+                if (gameBoard.spaces[6] == currentPlayer.mark && gameBoard.spaces[7] == currentPlayer.mark){
+                    return turn.innerText = `${currentPlayer.name} wins on the bottom`
+                }
+            }
+            if (gameBoard.spaces[4] === currentPlayer.mark) {
+                if (gameBoard.spaces[3] == currentPlayer.mark && gameBoard.spaces[5] == currentPlayer.mark){
+                    return turn.innerText = `${currentPlayer.name} wins in the middle`
+                }
+                if (gameBoard.spaces[1] == currentPlayer.mark && gameBoard.spaces[7] == currentPlayer.mark){
+                    return turn.innerText = `${currentPlayer.name} wins in the middle`
+                }
+            }
+        }
+        if(!gameBoard.spaces[(+id - 1)]){
+            gameBoard.spaces[(+id - 1)] = currentPlayer.mark
+            event.target.innerText = currentPlayer.mark
+
+            if(playerHasWon()) {
+                return turn.style.backgroundColor = "red"
+            }
+            currentPlayer = currentPlayer == player1 ? player2 : player1;
+            turn.innerText = `${currentPlayer.name}`
+        }
+        
     }
     
     gameBoardArr.forEach((pos, index) => {

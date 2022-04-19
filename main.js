c37b3c9df31c5a9268b3fc29332eade72fddd876
zoro-chi <jagbetuyi001@gmail.com>
2022-04-19 12:20:26 +0100

    added highlight winning blocks functionality

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ const game = (() => {
     const reset = () =>{
         gameBoard.spaces = [null, null, null, null, null, null, null, null, null]
         gameBoardArr.forEach(pos => pos.innerText = "")
+        gameBoardArr.forEach(pos => pos.style.backgroundColor = "initial")
         currentPlayer = player1
         turn.innerText = `${currentPlayer.name}`
         turn.style.backgroundColor = ""
@@ -45,6 +46,7 @@ const game = (() => {
     const hardReset = () =>{
         gameBoard.spaces = [null, null, null, null, null, null, null, null, null]
         gameBoardArr.forEach(pos => pos.innerText = "")
+        gameBoardArr.forEach(pos => pos.style.backgroundColor = "initial")
         currentPlayer = player1
         turn.innerText = `${currentPlayer.name}`
         turn.style.backgroundColor = ""
@@ -60,40 +62,121 @@ const game = (() => {
             if (gameBoard.spaces[0] === currentPlayer.mark) {
                 if (gameBoard.spaces[1] == currentPlayer.mark && gameBoard.spaces[2] == currentPlayer.mark){
                     winningPlayer = currentPlayer.name
+                    if (winningPlayer == player1.name){
+                        gameBoardArr[0].style.backgroundColor = "red"
+                        gameBoardArr[1].style.backgroundColor = "red"
+                        gameBoardArr[2].style.backgroundColor = "red"
+                    }
+                    if (winningPlayer == player2.name){
+                        gameBoardArr[0].style.backgroundColor = "blue"
+                        gameBoardArr[1].style.backgroundColor = "blue"
+                        gameBoardArr[2].style.backgroundColor = "blue"
+                    }
                     return turn.innerText = `${currentPlayer.name} wins up top`
                 }
                 if (gameBoard.spaces[3] == currentPlayer.mark && gameBoard.spaces[6] == currentPlayer.mark){
                     winningPlayer = currentPlayer.name
+                    if (winningPlayer == player1.name){
+                        gameBoardArr[0].style.backgroundColor = "red"
+                        gameBoardArr[3].style.backgroundColor = "red"
+                        gameBoardArr[6].style.backgroundColor = "red"
+                    }
+                    if (winningPlayer == player2.name){
+                        gameBoardArr[0].style.backgroundColor = "blue"
+                        gameBoardArr[3].style.backgroundColor = "blue"
+                        gameBoardArr[6].style.backgroundColor = "blue"
+                    }
                     return turn.innerText = `${currentPlayer.name} wins on the left`
                 }
                 if (gameBoard.spaces[4] == currentPlayer.mark && gameBoard.spaces[8] == currentPlayer.mark){
                     winningPlayer = currentPlayer.name
+                    if (winningPlayer == player1.name){
+                        gameBoardArr[0].style.backgroundColor = "red"
+                        gameBoardArr[4].style.backgroundColor = "red"
+                        gameBoardArr[8].style.backgroundColor = "red"
+                    }
+                    if (winningPlayer == player2.name){
+                        gameBoardArr[0].style.backgroundColor = "blue"
+                        gameBoardArr[4].style.backgroundColor = "blue"
+                        gameBoardArr[8].style.backgroundColor = "blue"
+                    }
                     return turn.innerText = `${currentPlayer.name} wins diagonally`
                 }
             }
             if (gameBoard.spaces[8] === currentPlayer.mark){
                 if (gameBoard.spaces[2] == currentPlayer.mark && gameBoard.spaces[5] == currentPlayer.mark){
                     winningPlayer = currentPlayer.name
+                    if (winningPlayer == player1.name){
+                        gameBoardArr[8].style.backgroundColor = "red"
+                        gameBoardArr[2].style.backgroundColor = "red"
+                        gameBoardArr[5].style.backgroundColor = "red"
+                    }
+                    if (winningPlayer == player2.name){
+                        gameBoardArr[8].style.backgroundColor = "blue"
+                        gameBoardArr[2].style.backgroundColor = "blue"
+                        gameBoardArr[5].style.backgroundColor = "blue"
+                    }
                     return turn.innerText = `${currentPlayer.name} wins on the right`
                 }
                 if (gameBoard.spaces[6] == currentPlayer.mark && gameBoard.spaces[7] == currentPlayer.mark){
                     winningPlayer = currentPlayer.name
+                    if (winningPlayer == player1.name){
+                        gameBoardArr[8].style.backgroundColor = "red"
+                        gameBoardArr[6].style.backgroundColor = "red"
+                        gameBoardArr[7].style.backgroundColor = "red"
+                    }
+                    if (winningPlayer == player2.name){
+                        gameBoardArr[8].style.backgroundColor = "blue"
+                        gameBoardArr[6].style.backgroundColor = "blue"
+                        gameBoardArr[7].style.backgroundColor = "blue"
+                    }
                     return turn.innerText = `${currentPlayer.name} wins on the bottom`
                 }
+                
             }
             if (gameBoard.spaces[4] === currentPlayer.mark) {
                 if (gameBoard.spaces[3] == currentPlayer.mark && gameBoard.spaces[5] == currentPlayer.mark){
                     winningPlayer = currentPlayer.name
+                    if (winningPlayer == player1.name){
+                        gameBoardArr[4].style.backgroundColor = "red"
+                        gameBoardArr[3].style.backgroundColor = "red"
+                        gameBoardArr[5].style.backgroundColor = "red"
+                    }
+                    if (winningPlayer == player2.name){
+                        gameBoardArr[4].style.backgroundColor = "blue"
+                        gameBoardArr[3].style.backgroundColor = "blue"
+                        gameBoardArr[5].style.backgroundColor = "blue"
+                    }
                     return turn.innerText = `${currentPlayer.name} wins in the middle`
                 }
                 if (gameBoard.spaces[1] == currentPlayer.mark && gameBoard.spaces[7] == currentPlayer.mark){
                     winningPlayer = currentPlayer.name
+                    if (winningPlayer == player1.name){
+                        gameBoardArr[4].style.backgroundColor = "red"
+                        gameBoardArr[1].style.backgroundColor = "red"
+                        gameBoardArr[7].style.backgroundColor = "red"
+                    }
+                    if (winningPlayer == player2.name){
+                        gameBoardArr[4].style.backgroundColor = "blue"
+                        gameBoardArr[1].style.backgroundColor = "blue"
+                        gameBoardArr[7].style.backgroundColor = "blue"
+                    }
                     return turn.innerText = `${currentPlayer.name} wins in the middle`
                 }
             }
             if (gameBoard.spaces[2] === currentPlayer.mark) {
                 if (gameBoard.spaces[4] == currentPlayer.mark && gameBoard.spaces[6] == currentPlayer.mark){
                     winningPlayer = currentPlayer.name
+                    if (winningPlayer == player1.name){
+                        gameBoardArr[2].style.backgroundColor = "red"
+                        gameBoardArr[4].style.backgroundColor = "red"
+                        gameBoardArr[6].style.backgroundColor = "red"
+                    }
+                    if (winningPlayer == player2.name){
+                        gameBoardArr[2].style.backgroundColor = "blue"
+                        gameBoardArr[4].style.backgroundColor = "blue"
+                        gameBoardArr[6].style.backgroundColor = "blue"
+                    }
                     return turn.innerText = `${currentPlayer.name} wins diagonally`
                 }
             }
@@ -104,14 +187,17 @@ const game = (() => {
             
             if(playerHasWon()) {
                 winningPlayer = currentPlayer.name
+                let winColor;
                 if (winningPlayer === "player1"){
                         player1Wins.innerText = Number(player1Wins.innerText) + 1
+                        winColor = "red"
                     }
                 if (winningPlayer === "player2"){
-                    player2Wins.innerText = Number(player2Wins.innerText) + 1
-                }
+                        player2Wins.innerText = Number(player2Wins.innerText) + 1
+                        winColor = "blue"
+                    }
                 setTimeout(reset, 3000)
-                return turn.style.backgroundColor = "red"
+                return turn.style.backgroundColor = winColor
                 
             }
             currentPlayer = currentPlayer === player1 ? player2 : player1;
